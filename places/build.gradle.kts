46455a5ca985623ad3cc32645bcba38afd166d61
Nicola Gallazzi <nicola.gallazzi.dev@gmail.com>
2024-09-04 11:39:08 +0200

    build: Version bump

--- places/build.gradle.kts
+++ places/build.gradle.kts
@@ -96,7 +96,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.ngallazzi",
         artifactId = "KMP-Places-Autocomplete",
-        version = "1.0.0"
+        version = "1.0.3"
     )
 
     // Configure POM metadata for the published artifact
